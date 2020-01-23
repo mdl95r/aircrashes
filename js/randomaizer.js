df7e0589217561d10656f6996e9cd6e916591c67
Artem Bogomolov <awesomemdl95@gmail.com>
2020-01-23 13:58:22 +0300

    Update randomaizer.js

--- js/randomaizer.js
+++ js/randomaizer.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
             setInterval(window.onload = function(){
                    var urls = [
-						"local:///main/stolknovenie-samoletov.html",
-						"local:///main/pozhar-v-samolete.html",
-						"local:///main/neispravnost-konstrukcii-v-samolete.html",
-						"local:///main/otkaz-dvigatelej.html",
+						"https://mdl95r.github.io/main/stolknovenie-samoletov.html",
+						"https://mdl95r.github.io/pozhar-v-samolete.html",
+						"https://mdl95r.github.io/main/neispravnost-konstrukcii-v-samolete.html",
+						"https://mdl95r.github.io/otkaz-dvigatelej.html",
 					];
 					document.getElementById("random-link").addEventListener("click", function(){
 						var url = urls[Math.floor(Math.random()*urls.length)]
