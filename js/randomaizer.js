51fcbaf97f7006220b5d27bf694d62c4b64ce010
Artem Bogomolov <awesomemdl95@gmail.com>
2020-01-23 13:41:30 +0300

    Update randomaizer.js

--- js/randomaizer.js
+++ js/randomaizer.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
             setInterval(window.onload = function(){
                    var urls = [
-						"D:/aircrashes.info/main/stolknovenie-samoletov.html",
-						"D:/aircrashes.info/main/pozhar-v-samolete.html",
-						"D:/aircrashes.info/main/neispravnost-konstrukcii-v-samolete.html",
-						"D:/aircrashes.info/main/otkaz-dvigatelej.html",
+						"local:///main/stolknovenie-samoletov.html",
+						"local:///main/pozhar-v-samolete.html",
+						"local:///main/neispravnost-konstrukcii-v-samolete.html",
+						"local:///main/otkaz-dvigatelej.html",
 					];
 					document.getElementById("random-link").addEventListener("click", function(){
 						var url = urls[Math.floor(Math.random()*urls.length)]
