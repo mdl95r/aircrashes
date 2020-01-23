eb135a52b0f1515831fe370321cf3c331f3ebab8
Artem Bogomolov <awesomemdl95@gmail.com>
2020-01-23 21:20:22 +0300

    Add files via upload

--- js/randomaizer.js
+++ js/randomaizer.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
             setInterval(window.onload = function(){
                    var urls = [
 						"https://mdl95r.github.io/main/stolknovenie-samoletov.html",
-						"https://mdl95r.github.io/pozhar-v-samolete.html",
+						"https://mdl95r.github.io/main/pozhar-v-samolete.html",
 						"https://mdl95r.github.io/main/neispravnost-konstrukcii-v-samolete.html",
-						"https://mdl95r.github.io/otkaz-dvigatelej.html",
+						"https://mdl95r.github.io/main/otkaz-dvigatelej.html",
 					];
 					document.getElementById("random-link").addEventListener("click", function(){
 						var url = urls[Math.floor(Math.random()*urls.length)]
